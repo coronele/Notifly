0575a863c31b5f41c9c5792b56996af3ef1e188e
claytonweaver <cweaver@cbctrenton.com>
2020-03-20 14:24:43 -0400

    added authorize

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NotiflyV0._1.Models;
@@ -13,6 +14,7 @@ using NotiflyV0._1.Models;
 
 namespace NotiflyV0._1.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly NotiflyDbContext _context;
